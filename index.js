52b8ef469ef89442975d52b9a2a4661be21c6559
sameerab04 <52090771+sameerab04@users.noreply.github.com>
2024-08-14 18:52:52 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Welcome to Sameera Boppana's Portfolio!");
+    // Example: Add more interactivity here, like event listeners or animations
+});
